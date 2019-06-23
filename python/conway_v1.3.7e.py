f7f231190bea9ccfbbf393c9a3d3fcd296b3405b
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2019-06-23 05:16:07 -0500

    multiple cell selection improved pause and fps system updated cursor removed due to performance

--- python/conway_v1.3.7e.py
+++ python/conway_v1.3.7e.py
@@ -1,88 +1,54 @@
-import sys
 import pygame
-import copy
-import random
+import sys
 import math
+from random import randint
+from copy import deepcopy
 from PIL import Image
 #--------------------------------------------------
-pygame.init()
 
 NUM_OF_CELLS = 25
-NUM_OF_PRESETS = 5
-WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 800, 800 # Dimensions of window
-
-SHORT_SIDE = min( WINDOW_WIDTH, WINDOW_HEIGHT ) # Short side of window
-CELL_SIZE = int( SHORT_SIDE / NUM_OF_CELLS ) # Side length of a cell
-GRID_SIZE = NUM_OF_CELLS * CELL_SIZE # Side length of grid
-
-#  Dynamic asset resizing - Open default cell design as PIL Image, 
-#   resize it, save it as new .png file, load as pygame.image
-# Resize full cell design
-image = Image.open( 'full_cell_original.png' )
-new_image = image.resize( ( CELL_SIZE, CELL_SIZE ) )
-new_image.save( 'full_cell_current.png' )
-# Resize empty cell design
-image = Image.open( 'empty_cell_original.png' )
-new_image = image.resize( ( CELL_SIZE, CELL_SIZE ) )
-new_image.save( 'empty_cell_current.png' )
-
-# Load in resized designs for use
-full_cell = pygame.image.load( 'full_cell_current.png' )
-empty_cell = pygame.image.load( 'empty_cell_current.png' )
-cursor = pygame.image.load( 'dan_cursor.png' )
-#  Random button unused
-# random_button = pygame.image.load( 'random_button.png' )
 
 #--------------------------------------------------
 def generation( c_grid ) :
   n = NUM_OF_CELLS
-  updated_grid = copy.deepcopy( c_grid )
+  updated_grid = deepcopy( c_grid )
 
   for y in range( 1, n+1 ) :
     for x in range( 1, n+1 ) :
       neighbors = 0
 
-      if c_grid[ x-1 ][ y-1 ] == 1 :
-        neighbors += 1
-      if c_grid[  x  ][ y-1 ] == 1 : 
-        neighbors += 1
-      if c_grid[ x+1 ][ y-1 ] == 1 : 
-        neighbors += 1
-      if c_grid[ x-1 ][  y  ] == 1 : 
-        neighbors += 1
-      if c_grid[ x+1 ][  y  ] == 1 : 
-        neighbors += 1
-      if c_grid[ x-1 ][ y+1 ] == 1 : 
-        neighbors += 1
-      if c_grid[  x  ][ y+1 ] == 1 : 
-        neighbors += 1
-      if c_grid[ x+1 ][ y+1 ] == 1 : 
-        neighbors += 1
+      neighbors += c_grid[ x-1 ][ y-1 ]
+      neighbors += c_grid[  x  ][ y-1 ]
+      neighbors += c_grid[ x+1 ][ y-1 ]
+      neighbors += c_grid[ x-1 ][  y  ]
+      neighbors += c_grid[ x+1 ][  y  ]
+      neighbors += c_grid[ x-1 ][ y+1 ]
+      neighbors += c_grid[  x  ][ y+1 ]
+      neighbors += c_grid[ x+1 ][ y+1 ]
 
       # Live cell
       if c_grid[ x ][ y ] == 1 :
         if neighbors < 2 :
           # Death by exposure ( <2 neighbors )
-      	  updated_grid[ x ][ y ] = 0
+          updated_grid[ x ][ y ] = 0
         elif neighbors > 3 :
           # Death by overcrowding ( >3 neighbors )
           updated_grid[ x ][ y ] = 0
       # Dead cell
       else :
-      	if neighbors == 3 :
-      	  # New life
-      	  updated_grid[ x ][ y ] = 1
+        if neighbors == 3 :
+          # New life
+          updated_grid[ x ][ y ] = 1
 
   return updated_grid
 #--------------------------------------------------
-def printGrid( screen, c_grid, v_grid ) :
-  #x and y coordinates are inverted 
+def printGrid( screen, c_grid, v_grid, full_cell, empty_cell ) :
   for y in range( NUM_OF_CELLS ) :
     for x in range( NUM_OF_CELLS ) :
       if c_grid[ x+1 ][ y+1 ] == 0 :
-        screen.blit( empty_cell, v_grid[ y ][ x ] )
+        screen.blit( empty_cell, v_grid[ x ][ y ] )
       else :
-        screen.blit( full_cell, v_grid[ y ][ x ] )
+        screen.blit( full_cell, v_grid[ x ][ y ] )
 #--------------------------------------------------
 def randomGrid() :
   # Create empty grid full of 0's
@@ -91,29 +57,40 @@ def randomGrid() :
   # Populate c_grid randomly
   for y in range( 1, NUM_OF_CELLS+1 ) :
     for x in range( 1, NUM_OF_CELLS+1 ) :
-      c_grid[x][y] = random.randint( 0, 1 )
+      c_grid[x][y] = randint( 0, 1 )
   return c_grid
 #--------------------------------------------------
 def loadPreset( choice ) :
   # Load in all presets as list
   with open( 'grid_presets.txt' ) as presets_file :
-    presets = presets_file.read().split( '\n' )	
+    presets = presets_file.read().split( '\n' )
+
+  # Error check
+  if choice > len( presets ) :
+    print ( 'Error: Choice number given does not exist' )
+    sys.exit( 1 )
 
   # Continue with only chosen preset as list of preset data
   preset = str( presets[ choice-1 ] )
   preset = preset.split( ',' )
+  name = preset[ 0 ]
+  col_num = int( preset[ 1 ] )
+  row_num = int( preset[ 2 ] )
   
   # Print name of preset to command line \TODO implement graphically
-  print( preset[ 0 ] )
+  # print( preset[ 0 ] )
+  # Debug assistance for preset creation
+  for i in range( len( preset ) ) :
+    print( '%2d  %s' % ( i, preset[ i ] ) )
 
   # Calculate values for creating visual grid
-  total_rows = NUM_OF_CELLS - int( preset[ 2 ] )
-  row_offset = math.ceil( total_rows/2 ), math.floor( total_rows/2 )
-  row_barrier = row_offset[ 0 ]+1, NUM_OF_CELLS - row_offset[ 1 ]
+  blank_rows = NUM_OF_CELLS - row_num
+  row_offsets = math.ceil( blank_rows/2 ), math.floor( blank_rows/2 )
+  row_barriers = row_offsets[ 0 ]+1, NUM_OF_CELLS - row_offsets[ 1 ]
 
-  total_cols = NUM_OF_CELLS - int( preset[ 1 ] )
-  col_offset = math.ceil( total_cols/2 ), math.floor( total_cols/2 )
-  col_barrier = col_offset[ 0 ]+1, NUM_OF_CELLS - col_offset[ 1 ]
+  blank_cols = NUM_OF_CELLS - col_num
+  col_offsets = math.ceil( blank_cols/2 ), math.floor( blank_cols/2 )
+  col_barriers = col_offsets[ 0 ]+1, NUM_OF_CELLS - col_offsets[ 1 ]
 
   # Create a 12 by 12 grid of 0's
   c_grid = [ [ 0 ] * ( NUM_OF_CELLS+2 ) for _ in range( NUM_OF_CELLS+2 ) ]
@@ -122,89 +99,133 @@ def loadPreset( choice ) :
   for y in range( 1, NUM_OF_CELLS+1 ) :
     i = 0
     for x in range( 1, NUM_OF_CELLS+1 ) :
-      if y < row_barrier[ 0 ] :
+      if y < row_barriers[ 0 ] :
         c_grid[ x ][ y ] = 0
-      elif y > row_barrier[ 1 ] :
+      elif y > row_barriers[ 1 ] :
         c_grid[ x ][ y ] = 0
-      elif x < col_barrier[ 0 ] :
+      elif x < col_barriers[ 0 ] :
         c_grid[ x ][ y ] = 0
-      elif x > col_barrier[ 1 ] :
+      elif x > col_barriers[ 1 ] :
         c_grid[ x ][ y ] = 0
       else :
         c_grid[ x ][ y ] = int( preset[ line_num ][ i ] )
         i += 1
-    if not ( y < col_barrier[ 0 ] or y > col_barrier[ 1 ] ) :
+    if ( ( y <= row_barriers[ 1 ] ) and ( y >= row_barriers[ 0 ] ) ) :
       line_num += 1
   return c_grid
 #--------------------------------------------------
 def choseStartingGrid( choice ) :
   if choice == 0 :
     return randomGrid()
-  elif choice < 0 or choice > NUM_OF_PRESETS :
-    print ( 'Invalid Choice' )
+  elif choice < 0 :
+    print ( 'Error: Negative number for choice' )
     sys.exit( 1 )
   else :
     return loadPreset( choice )
 #--------------------------------------------------
 def menu() :
-
-
   pass
   # patternSelected = None
     # while(patternSelected == None):
 #--------------------------------------------------
-def setupWindow() :
+def setupWindow( window_size ) :
   #window setup
-  pygame.display.set_icon( full_cell )
+  icon = pygame.image.load( 'full_cell_original.png' )
+  pygame.display.set_icon( icon )
   pygame.display.set_caption( 'game of life' )
-  screen = pygame.display.set_mode( (800,800), )
+  screen = pygame.display.set_mode( window_size )
 
   # Make screen white
   screen.fill( [ 255, 255, 255 ] )
 
+  # Make operating system mouse invisible
+  # pygame.mouse.set_visible( False )
+
   return screen
 #--------------------------------------------------
+def loadAssets( cell_size, palette ) :
+  #  Dynamic asset resizing - Open default cell design as PIL Image, 
+  #   resize it, save it as new .png file, load as pygame.image
+  # Resize full cell design
+  image = Image.open( 'full_cell_' + palette + '.png' )
+  new_image = image.resize( ( cell_size, cell_size ) )
+  new_image.save( 'full_cell_current.png' )
+  # Resize empty cell design
+  image = Image.open( 'empty_cell_' + palette + '.png' )
+  new_image = image.resize( ( cell_size, cell_size ) )
+  new_image.save( 'empty_cell_current.png' )
+
+  # Load in resized designs for use
+  full_cell = pygame.image.load( 'full_cell_current.png' )
+  empty_cell = pygame.image.load( 'empty_cell_current.png' )
+  cursor = pygame.image.load( 'dan_cursor.png' )
+  #  Random button unused
+  # random_button = pygame.image.load( 'random_button.png' )
+  return full_cell, empty_cell, cursor
+#--------------------------------------------------
 def main() :
+  pygame.init()
+
+  # Dimensions of window
+  window_size = window_width, window_height = 800, 800
+  screen = setupWindow( window_size )
+  
+  # Compute size of cell in pixels for later usage
+  short_side = min( window_width, window_height )
+  cell_size = int( short_side / NUM_OF_CELLS )
+
+  # Display menu - not implemented
+  menu()
+
+  # Load assets according to specified palette
+  full_cell, empty_cell, cursor = loadAssets( cell_size, 'gameboy' )
+
   # Create empty grid of size NUM_OF_CELLS for display
   v_grid = [ [ 0 ] * NUM_OF_CELLS for _ in range( NUM_OF_CELLS ) ]
+
   # Compute offsets to print grid in the center of the window
-  horizontal_offset = ( WINDOW_WIDTH - GRID_SIZE )/2
-  vertical_offset = ( WINDOW_HEIGHT - GRID_SIZE )/2
-  # Fill v_grid with positions of each cell
+  grid_size = NUM_OF_CELLS * cell_size # Side length of grid
+  horizontal_offset = ( window_width - grid_size )/2
+  vertical_offset = ( window_height - grid_size )/2
+
+  # Fill v_grid with coordinates of each cell
   for x in range( NUM_OF_CELLS ):
     for y in range( NUM_OF_CELLS ):
-      v_grid[ x ][ y ] = empty_cell.get_rect().move( ( x * CELL_SIZE ) + horizontal_offset, ( y * CELL_SIZE ) + vertical_offset )
-      
+      v_grid[ x ][ y ] = empty_cell.get_rect().move( ( x * cell_size ) + horizontal_offset, ( y * cell_size ) + vertical_offset )
+  
+  # Load preset for grid based on command line input
   choice = int( sys.argv[ 1 ] )
   c_grid = choseStartingGrid( choice )
 
   # Get initial cursor position
   cursor_pos = pygame.mouse.get_pos()
 
-  # pygame.mouse.set_visible( False )
-
-  screen = setupWindow()
-  
-  # Display menu - not implemented
-  menu()
-
-  # The frequency of generations per each execution of the main loop
-  # Ex: if generation_frequency is exactly 7 then generations occur 1 out of every 7 loops
-  generation_frequency = 2
-  frequency_count = 0
-  generation_flag = True
-
-  # multiple cell selection
-  current_cell_selection = list()
-  unique_list = list()
-  cell_selection = False
-
-  #pause mode
+  # new pause mode
   clock = pygame.time.Clock()
   pause_game = False
+
+  # multiple cell selection
+  cell_selection_dict = dict()
+  cell_selection = False
+
   while 1:
-    # sets fps
-    clock.tick(60)
+
+    # set fps
+    clock.tick(120)
+
+    # Clear screen with background grey
+    screen.fill( [ 15, 56, 15 ] )
+
+    # Display visual grid based on computational grid
+    printGrid( screen, c_grid, v_grid, full_cell, empty_cell )
+
+    # Display selection cursor
+    # screen.blit( cursor, cursor_pos )
+
+    # Update entire window
+    pygame.display.flip()
+
+
     # Event watchdog
     for event in pygame.event.get() :
       # Watch for exit button press
@@ -217,78 +238,65 @@ def main() :
         elif event.key == pygame.K_p :
           # generation_flag = not generation_flag
           pause_game = not pause_game
-
+        elif event.key == pygame.K_c :
+          #clear grid
+          for y in range(NUM_OF_CELLS+1):
+            for x in range(NUM_OF_CELLS+1):
+              c_grid[x][y] = 0 
+      
       # Watch for mouse movement
       if event.type == pygame.MOUSEMOTION :
-        if cell_selection:
-          
+        cursor_pos = pygame.mouse.get_pos()
+
+        if cell_selection:  
           clicked_pos = pygame.mouse.get_pos()
-          cell_clicked = int( ( clicked_pos[ 0 ] - horizontal_offset ) / CELL_SIZE )+1, int( ( clicked_pos[ 1 ] - vertical_offset ) / CELL_SIZE )+1
-          current_cell_selection.append(cell_clicked)
+          cell_clicked = int( ( clicked_pos[ 0 ] - horizontal_offset ) / cell_size )+1, int( ( clicked_pos[ 1 ] - vertical_offset ) / cell_size )+1
+   
+          # update if valid position and coordinate is not already in dictionary
+          if ( cell_clicked[ 1 ] <= NUM_OF_CELLS and cell_clicked[ 0 ] <= NUM_OF_CELLS ) and ( cell_clicked not in cell_selection_dict.keys() ):
+            cell_selection_dict.update({ cell_clicked : c_grid[cell_clicked[0]][cell_clicked[1]] })
 
-          for cell in current_cell_selection:
-            if cell not in unique_list:
-              unique_list.append(cell)
-          
-          # current_cell = unique_list.pop(0)
-          while(len(unique_list) > 0):
-            if( unique_list[0][ 1 ] <= NUM_OF_CELLS and unique_list[0][ 0 ] <= NUM_OF_CELLS ) :
-                if c_grid[ unique_list[0][ 1 ] ][ unique_list[0][ 0 ] ] == 1 :
-                  c_grid[ unique_list[0][ 1 ] ][ unique_list[0][ 0 ] ] = 0
-                else:
-                  c_grid[ unique_list[0][ 1 ] ][ unique_list[0][ 0 ] ] = 1
-            unique_list.pop(0)
 
+          # invert cell state to indicate selection
+          for coordinate in cell_selection_dict.keys():
+            if c_grid[coordinate[0]][coordinate[1]] == 1:
+              c_grid[coordinate[0]][coordinate[1]] = 0
+            else:
+              c_grid[coordinate[0]][coordinate[1]] = 1
 
       # Watch for mouse clicks
-      if event.type == pygame.MOUSEBUTTONDOWN:
+      if event.type == pygame.MOUSEBUTTONDOWN :
         cell_selection = True
         clicked_pos = pygame.mouse.get_pos()
         # Determine which cell was clicked if one was clicked
-        cell_clicked = int( ( clicked_pos[ 0 ] - horizontal_offset ) / CELL_SIZE )+1, int( ( clicked_pos[ 1 ] - vertical_offset ) / CELL_SIZE )+1
-        #x and y coordinates are inverted
+        cell_clicked = int( ( clicked_pos[ 0 ] - horizontal_offset ) / cell_size )+1, int( ( clicked_pos[ 1 ] - vertical_offset ) / cell_size )+1
+        
         if( cell_clicked[ 1 ] <= NUM_OF_CELLS and cell_clicked[ 0 ] <= NUM_OF_CELLS ) :
-          if c_grid[ cell_clicked[ 1 ] ][ cell_clicked[ 0 ] ] == 1 :
-            c_grid[ cell_clicked[ 1 ] ][ cell_clicked[ 0 ] ] = 0
+          if c_grid[ cell_clicked[ 0 ] ][ cell_clicked[ 1 ] ] == 1 :
+            c_grid[ cell_clicked[ 0 ] ][ cell_clicked[ 1 ] ] = 0
           else:
-            c_grid[ cell_clicked[ 1 ] ][ cell_clicked[ 0 ] ] = 1
-      
+            c_grid[ cell_clicked[ 0 ] ][ cell_clicked[ 1 ] ] = 1
+
       # watch for selection end
       if event.type == pygame.MOUSEBUTTONUP:
+        # print(cell_selection_dict.keys())
+        for coordinate in cell_selection_dict.keys():
+          # turn cell on 
+          c_grid[coordinate[0]][coordinate[1]] = 1
+          # invert cell state saved
+          # if cell_selection_dict.get(coordinate) == 1:
+          #   c_grid[coordinate[0]][coordinate[1]] = 0
+          # else:
+          #   c_grid[coordinate[0]][coordinate[1]] = 1
+      
+
+        cell_selection_dict.clear()
         cell_selection = False
 
-        for cell in current_cell_selection:
-          print(cell)
-        # unique_list.clear()
-        current_cell_selection.clear()
+    # execute generation(...) if game is not paused
+    if pause_game  == False:
+      c_grid = deepcopy( generation( c_grid ) )
 
-
-    # Clear screen with background grey
-    screen.fill( [ 128, 128, 128 ] )
-
-    # Display visual grid based on computational grid
-    printGrid( screen, c_grid, v_grid )
-
-    # Display selection cursor
-    # screen.blit( cursor, cursor_pos )
-
-    # Only execute generation(...) 1 out of every generation_frequency times and handle pauses
-    # if frequency_count == generation_frequency and generation_flag :
-    #   # Update computational grid based on game of life rules. One generation
-    #   c_grid = copy.deepcopy( generation( c_grid ) )
-    #   frequency_count = 1
-    # elif generation_flag :
-    #   frequency_count += 1
-    # else :
-    #   pass
-    if not pause_game:
-      c_grid = copy.deepcopy( generation( c_grid ) )
-
-    # Update entire window
-    pygame.display.flip()
-
-    # Control time of each generation by releasing CPU for 40ms
-    # pygame.time.wait( 40 )
 #--------------------------------------------------
 if ( __name__ == '__main__' ) :
   main()
